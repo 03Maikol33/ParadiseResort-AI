3fed041b5d73ed79c8876b01a49e93be8f4f60d3
03Maikol33 <117536575+03Maikol33@users.noreply.github.com>
2026-07-20 18:51:29 +0200

    risolto problema duplicati

--- include/page.inc.php
+++ include/page.inc.php
@@ -84,9 +84,9 @@ function new_block(string $template): Template {
     if (!empty($_SESSION['user']['id'])) {
         $block->setContentOnce('is_logged', '1');
         $uName = $_SESSION['user']['name'] ?? trim(($_SESSION['user']['first_name'] ?? '') . ' ' . ($_SESSION['user']['last_name'] ?? ''));
-        $block->setContent('user_name', htmlspecialchars($uName));
+        $block->setContentOnce('user_name', htmlspecialchars($uName));
         if (!empty($_SESSION['user']['role_path'])) {
-            $block->setContent('role_path', $_SESSION['user']['role_path']);
+            $block->setContentOnce('role_path', $_SESSION['user']['role_path']);
         }
     } else {
         $block->setContentOnce('is_logged', '');
